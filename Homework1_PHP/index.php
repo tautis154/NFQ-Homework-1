f03c2edbd77d32f50a1ac48a2e70ebbbdb2c1cb7
tautis154 <36332264+tautis154@users.noreply.github.com>
2019-10-15 00:16:09 +0300

    Add files via upload
    Fixed index.php

--- Homework1_PHP/index.php
+++ Homework1_PHP/index.php
@@ -1,9 +1,9 @@
 <?php
-namespace Homework1_PHP;
+namespace HomeWork1_PHP;
 error_reporting(0);
 require_once 'vendor/autoload.php';
 
-function calculateHomeWorkSum(int...$numbers): int{
+function calculateHomeWorkSum(...$numbers){
     $homeWorkSum = 0;
     foreach( $numbers as $number){
     $homeWorkSum = $homeWorkSum + $number;
@@ -11,16 +11,8 @@ function calculateHomeWorkSum(int...$numbers): int{
     return $homeWorkSum;
 }
 
-//If there is a need for colored text
-//$climate = new \League\CLImate\CLImate;
-//$climate->blue("calculateHomeWorkSum: " .calculateHomeWorkSum(3,2.2,'1') . "\n");
-//$climate->red("Nfq\Akademija\NotTyped\calculateHomeWorkSum: " .\Nfq\Akademija\NotTyped\calculateHomeWorkSum(3,2.2,'1') . "\n");
-//$climate->yellow("Nfq\Akademija\Soft\calculateHomeWorkSum: " .\Nfq\Akademija\Soft\calculateHomeWorkSum(3,2.2,'1') . "\n");
-//$climate->green("Nfq\Akademija\Strict\calculateHomeWorkSum: " . \Nfq\Akademija\Strict\calculateHomeWorkSum(3,2.2,'1') . "\n");
-
-echo "calculateHomeWorkSum: " .calculateHomeWorkSum(3,2.2,'1') . "\n";
-echo "Nfq\Akademija\NotTyped\calculateHomeWorkSum: " .\Nfq\Akademija\NotTyped\calculateHomeWorkSum(3,2.2,'1') . "\n";
-echo "Nfq\Akademija\Soft\calculateHomeWorkSum: " .\Nfq\Akademija\Soft\calculateHomeWorkSum(3,2.2,'1') . "\n";
-echo "Nfq\Akademija\Strict\calculateHomeWorkSum: " .\Nfq\Akademija\Strict\calculateHomeWorkSum(3,2.2,'1') . "\n";
-
-
+$climate = new \League\CLImate\CLImate;
+$climate->orange("calculateHomeWorkSum: " .calculateHomeWorkSum(3,2.2,'1'));
+$climate->red("Nfq\Akademija\NotTyped\calculateHomeWorkSum: " .\Nfq\Akademija\NotTyped\calculateHomeWorkSum(3,2.2,'1'));
+$climate->yellow("Nfq\Akademija\Soft\calculateHomeWorkSum: " .\Nfq\Akademija\Soft\calculateHomeWorkSum(3,2.2,'1'));
+$climate->green("Nfq\Akademija\Strict\calculateHomeWorkSum: " . \Nfq\Akademija\Strict\calculateHomeWorkSum(3,2.2,'1'));
